e49e4ad12b04f4498961efbf524d4b8f97a33149
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2023-01-12 23:48:29 +0100

    small fixes to spec file

--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -1,4 +1,4 @@
-require './lib/calculator.rb'
+require './lib/calculator'
 
 describe Calculator do
   describe '#add' do
@@ -7,30 +7,30 @@ describe Calculator do
       expect(calculator.add(5, 2)).to eql(7)
     end
 
-    it 'return the sum of more than two numbers' do
+    it 'returns the sum of more than two numbers' do
       calculator = Calculator.new
       expect(calculator.add(2, 5, 7)).to eql(14)
     end
   end
 
   describe '#multiply' do
-    it 'return product of two numbers' do
+    it 'returns the product of two numbers' do
       calculator = Calculator.new
       expect(calculator.multiply(9, 2)).to eql(18)
     end
 
-    it 'return product of more than two numbers' do
+    it 'returns the product of more than two numbers' do
       calculator = Calculator.new
       expect(calculator.multiply(9, 2, 3)).to eql(54)
     end
   end
 
   describe '#subtract' do
-    it 'return quotent of two numbers' do
+    it 'returns the quotent of two numbers' do
       calculator = Calculator.new
       expect(calculator.subtract(10, 2)).to eql(5)
     end
-    it 'return quotent of more than two numbers' do
+    it 'returns the quotent of more than two numbers' do
       calculator = Calculator.new
       expect(calculator.subtract(20, 5, 2)).to eql(2)
     end
